53e2eb4e73ffc0bda0ff7b887ab194da13a7a7a5
Gagan <gaganpreetsinghmohal@gmail.com>
2026-02-22 22:29:36 +0530

    Added the improved version of Home Banner Layout

--- Netflix_Frontend/src/Components/Movie/MovieBanner.jsx
+++ Netflix_Frontend/src/Components/Movie/MovieBanner.jsx
@@ -4,8 +4,11 @@ function MovieBanner(){
     return(
         <>
 
-        <div className="relative w-screen h-screen">
+        <div className="relative h-screen w-screen flex justify-center items-end md:justify-start  ">
+
             <div className="    
+             h-[clamp(500px,100vh,580px)]
+             md:h-[clamp(500px,90vh,800px)]
              bg-[url('/images/banner/hero_banner.jpg')]  
              bg-no-repeat
              bg-cover 
@@ -13,38 +16,40 @@ function MovieBanner(){
              mask-r-from-90%
              absolute
              inset-0
-            " />
-             <div className=" w-1/2 h-2/3 absolute bottom-0  flex flex-col gap-2  ">
-                <div className="leading-none font-semibold">                
-                <h1 className="text-white text-[7rem] text-center" >THE</h1>
-                <h1 className="text-white text-[7rem] text-center" >PROTECTOR</h1>
+            bg-center
+            "> </div>
+
+             <div className=" w-1/2 h-[50%] md:h-[55%] absolute flex flex-col gap-2  ">
+                <div className="leading-none font-semibold  text-[clamp(2rem,10vw,5rem)]">    
+
+                <h1 className="text-white  text-center " >THE</h1>
+                <h1 className="text-white  text-center" >PROTECTOR</h1>
                 </div>
 
-                <div className="flex justify-center">
-                <p className="text-white max-w-[80%] text-[1rem] font-semibold">
+                <div className=" justify-center hidden  md:flex ">
+                <p className="text-white max-w-[90%]  text-[clamp(1rem,1vw,2rem)] font-semibold">
                     The city hides secrets older than memory.Power has chosen him.
                     Now destiny demands everything. Beneath the skyline of Istanbul, an ancient war awakens.He never asked for this power. But he will fight to protect what matters.
                 </p>
                 
                 </div>
 
-                 <div className="flex gap-3 m-4 ml-20">
-                <button className="py-2 px-8 bg-white text-small font-bold flex gap-3 items-center">
+                 <div className="flex w-full gap-3 justify-center ">
+               <button className="py-2.5 px-6 md:py-4 m bg-white text-small font-bold flex gap-2 cursor-pointer items-center">
                     <img src="/images/Vector.svg" alt="" className="w-3.5"/>
                     <p>Play</p>
-                    </button>
-                <button className="py-4 px-16 font-bold text-small flex gap-3 items-center bg-white/30">
+                </button>
+                <button className="py-2.5 md:py-4 px-16 font-bold text-small flex gap-2 items-center cursor-pointer bg-white/30">
                     <img src="/images/Detail.svg" alt="" className="w-5" />
                     <p className="text-white">Details</p>
 
-                    </button>
+                </button>
                 
                 </div>
-               
+               </div>
 
              </div>   
-                
-            </div>
+           
         </>
     )
 }
